6796eaebbb3b06040115026896203160025b2345
AlexanderGamarra <iepvagamarra@gmail.com>
2026-03-23 16:43:08 -0500

    feature: select menu automated

---
+++ src/tests/question_two.spec.ts
@@ -0,0 +1,17 @@
+import {test, expect} from '@playwright/test'
+import {writeAndselectValue} from '../utils/util.js'
+
+const SELECT_VALUE = 'Group 1, option 1'
+const SELECT_ONE_VALUE = 'Mr.'
+
+test("Menu Validations", async({page}) => {
+    await page.goto("https://demoqa.com/select-menu")
+    await expect(page.getByRole('heading', { name: 'Select Menu' })).toBeVisible()
+
+    await writeAndselectValue(page, '#react-select-2-input', SELECT_VALUE) 
+    await writeAndselectValue(page, '#react-select-3-input', SELECT_ONE_VALUE)
+    await page.locator('#oldSelectMenu').selectOption({ label: 'Purple' });
+    await writeAndselectValue(page, '#react-select-4-input', "Green")
+    await writeAndselectValue(page, '#react-select-4-input', "Blue")
+    await page.locator('#cars').selectOption({ label: 'Audi' });
+})
